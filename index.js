06448f2f030d3758057b8538e57948b64c720c69
Fabio Souza <fabiobarboza7@hotmail.com>
2018-07-11 02:59:06 -0300

    refactoring files

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import { Router, Scene } from 'react-native-router-flux';
 
-import Principal from './src/components/Principal';
-import SobreJogo from './src/components/SobreJogo';
-import OutrosJogos from './src/components/OutrosJogos';
+import Rotas from './src/Rotas';
 
 export default class app6 extends Component {
   render() {
-    return(
-      <Router>
-        <Scene key='app'>
-          <Scene key='principal' component={Principal} title="Cara ou Coroa"/>
-          <Scene key='sobrejogo' component={SobreJogo} title="Sobre o Jogo"/>
-          <Scene key='outrosjogos' component={OutrosJogos} title="Outros Jogos"/>
-        </Scene>
-      </Router>
+    return (
+      <Rotas />
     );
   }
 }
